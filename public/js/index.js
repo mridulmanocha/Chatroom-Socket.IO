9726888e8f9272f2adf20df22f83d5b475792df3
mridulmanocha <mridul.manocha22@gmail.com>
2019-04-19 16:56:00 +0530

    message events added

---
+++ public/js/index.js
@@ -0,0 +1,19 @@
+var socket = io();
+
+socket.on('connect' , function() {
+	console.log('connected to the server');
+});
+
+socket.on('disconnect' , function() {
+	console.log('disconnected from the server')
+});
+
+socket.on('newMessage' , function(message) {
+	console.log('new message' , message);
+});
+
+socket.emit('createMessage' , {
+		to : 'mridul@gmail.com',
+		body : 'hello billu',
+		createdAt : 123	
+	});
--- server/server.js
+++ server/server.js
@@ -5,18 +5,30 @@ const http = require('http');
 
 const publicPath = path.join(__dirname , '../public');
 const port = process.env.PORT || 3000;
-console.log(publicPath);
+
 
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
 
 io.on('connection' , (socket) => {
+
 	console.log('new user connected');
 
 	socket.on('disconnect' , () => {
 	console.log('user disconnected');
-})
+	});
+
+	socket.emit('newMessage' , {
+		from : 'mridul@gmail.com',
+		body : 'hello',
+		createdAt : 123	
+	});
+
+	socket.on('createMessage' , function(message) {
+	console.log('create message' , message);
+});
+
 });
 
 
