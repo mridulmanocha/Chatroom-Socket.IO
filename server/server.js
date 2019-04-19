1ccca9fc47028ca3b7556123261b4f0774e90976
mridulmanocha <mridul.manocha22@gmail.com>
2019-04-19 15:41:28 +0530

    initial commit

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const express = require('express');
+
+const publicPath = path.join(__dirname , '../public');
+const port = process.env.PORT || 3000;
+console.log(publicPath);
+
+var app = express();
+
+app.use(express.static(publicPath));
+
+app.listen(port ,() => {
+	console.log(`App is up and running on port ${port}.`);
+});
