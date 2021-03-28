a90feca9782fe8321d0b340f3cb34c4705b22535
Mridul Manocha <mridulmanocha@Mriduls-MacBook-Pro.local>
2021-03-28 17:20:23 +0530

    initial

--- server/server.js
+++ server/server.js
@@ -4,11 +4,6 @@ const socketIO = require('socket.io');
 const http = require('http');
 const https = require('https');
 
-
-const {generateMessage} = require('./utils/message');
-const {generateLocationMessage} = require('./utils/message');
-
-
 const publicPath = path.join(__dirname , '../public');
 const port = process.env.PORT || 3000;
 
@@ -16,38 +11,67 @@ var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
 
+var count = 0;
+
+var $ipsConnected = [];
 
 
-io.on('connection' , (socket) => {
 
-	console.log('new user connected');
+io.on('connection', function (socket) {
 
-	socket.on('disconnect' , () => {
-	console.log('user disconnected');
-	});
 
-	socket.emit('newMessage' , generateMessage('Mridul Manocha' , 'Welcome to my chatroom'));
 
-	socket.broadcast.emit('newMessage' , generateMessage('Admin' , 'New user joined'));
+//   var $ipAddress = socket.handshake.address;
 
-	socket.on('createMessage' , (message, callback) => {
-	console.log('create message' , message);
-	io.emit('newMessage' , generateMessage(message.from, message.body));
+//   if (!$ipsConnected.hasOwnProperty($ipAddress)) {
 
-	callback('this is from server, message received.');
+//   	$ipsConnected[$ipAddress] = 1;
+
+//   	count++;
+
+//   	socket.emit('counter', {count:count});
+
+//   }
+
+	count++;
+
+  	socket.emit('counter', {count:count});
+
+
+
+  console.log("client is connected");
+
+
+
+  /* Disconnect socket */
+
+  socket.on('disconnect', function() {
+
+  	// if ($ipsConnected.hasOwnProperty($ipAddress)) {
+
+  	// 	delete $ipsConnected[$ipAddress];
+
+	//     count--;
+
+	//     socket.emit('counter', {count:count});
+
+  	// }
+
+	  count--;
+
+	    socket.emit('counter', {count:count});
+
+		console.log("client is disconnected");
+
+  });
+
+
+
 });
-
-	
-	socket.on('createLocation' , (coords) => {
-		io.emit('newLocationMessage' , generateLocationMessage('New User' , coords.longitude , coords.latitude));
-	});
-
-});
-
-
 
 app.use(express.static(publicPath));
 
-server.listen(port ,() => {
-	console.log(`App is up and running on port ${port}.`);
+
+server.listen(3000,()=>{
+	console.log('listening at 3000')
 });
